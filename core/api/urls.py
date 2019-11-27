00aef04279daa0bc77d7631c7f1d2f6840ce03d6
ytjh05 <46480817+ytjh05@users.noreply.github.com>
2019-11-27 15:21:30 +0900

    modify style & add team and player code

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-from core.api.views import PlayerStats,scheduler
-urlpatterns = [path("player-stats/<str:name>/", PlayerStats.as_view())]
-urlpatterns = [path("scheduler/<str:name>&<str:period>/", scheduler.as_view())]
+from core.api.views import PlayerStats, scheduler
+urlpatterns = [
+    path("player-stats/<str:name>/", PlayerStats.as_view()),
+    path("scheduler/<str:name>&<str:period>&<str:period2>/", scheduler.as_view()),
+]
--- core/api/views.py
+++ core/api/views.py
@@ -1,5 +1,4 @@
 import statsapi
-import datetime
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -31,14 +30,49 @@ class PlayerStats(APIView):
         return Response(player)
 class scheduler(APIView):
     permission_classes = [AllowAny]
-    team_id = {
+    id = {
+        "Los Angeles Angels": 108,
+        "Arizona Diamondbacks": 109,
+        "Baltimore Orioles": 110,
+        "Boston Red Sox": 111,
+        "Chicago Cubs": 112,
+        "Cincinnati Reds": 113,
+        "Cleveland Indians": 114,
+        "Colorado Rockies": 115,
+        "Detroit Tigers": 116,
+        "Houston Astros": 117,
+        "Kansas City Royals": 118,
+        "Los Angeles Dodgers": 119,
+        "Washington Nationals": 120,
+        "New York Mets": 121,
+        "Oakland Athletics": 133,
+        "Pittsburgh Pirates": 134,
+        "San Diego Padres": 135,
+        "Seattle Mariners": 136,
+        "San Francisco Giants": 137,
+        "St. Louis Cardinals": 138,
+        "Tampa Bay Rays": 139,
         "Texas Rangers": 140,
+        "Toronto Blue Jays": 141,
+        "Minnesota Twins": 142,
+        "Philadelphia Phillies": 143,
+        "Atlanta Braves": 144,
+        "Chicago White Sox": 145,
+        "Miami Marlins": 146,
+        "New York Yankees": 147,
+        "Milwaukee Brewers": 158,
+        "Shin-Soo Choo": 140,
+        "Hyun-Jin Ryu": 110,
+        "Jung Ho": 134,
+        "Seunghwan": 115,
+        "Ji-Man": 139,
     }
-    def get(self, request, name, period):## 팀이름 받고 기간을 정해주면 그안에 있는거 다 뽑기
-        nowDate = datetime.datetime.now().strftime('%Y-%m-%d')
-        teamschedule = statsapi.schedule(start_date=nowDate, end_date=period, team=self.team_id[name])
+    def get(self, request, name, period, period2):## 팀이름 받고 기간을 정해주면 그안에 있는거 다 뽑기
+        team_schedule = statsapi.schedule(
+            start_date=period, end_date=period2, team=self.id[name]
+        )
         dates = []
-        for input in range(0, len(teamschedule)-1):
-            dates.append(teamschedule[input]["summary"])
+        for schedule in range(0, len(team_schedule)-1):
+            dates.append(team_schedule[schedule]["summary"])
         return Response(dates)
 

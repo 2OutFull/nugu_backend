7ef9dab103faac910139b5cde52d0b4f33360572
YoungHoon Lee <nobel6018@gmail.com>
2019-12-13 02:06:17 +0900

    Update: add team schedule bug fix

--- core/api/urls.py
+++ core/api/urls.py
@@ -5,6 +5,6 @@ from core.api.views import PlayerStats, Scheduler, NextGame
 urlpatterns = [
     path("pitcher-stat", PlayerStats.as_view()),
     path("hitter-stat", PlayerStats.as_view()),
-    path("scheduler", Scheduler.as_view()),
+    path("schedule", Scheduler.as_view()),
     path("nextgame", NextGame.as_view())
 ]
--- core/api/views.py
+++ core/api/views.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime, timedelta
 
+import requests
 import statsapi
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -99,38 +100,63 @@ class Scheduler(APIView):
 
     def post(self, request):
         data = request.data["action"]
-        request_team = data["parameters"]["team"]["value"]
+        request_team = data["parameters"]["team_name"]["value"]
 
         with open("configure_package/mlb_team_list.json") as mlb_team:
             mlb_team_list = json.load(mlb_team)
         if request_team in mlb_team_list:
             team_id = mlb_team_list[request_team]
+
+            time = datetime.now()
+            default_start_date = time.strftime("%Y-%m-%d")
+            default_end_date = (time + timedelta(days=90)).strftime("%Y-%m-%d")
+            team_schedule = statsapi.schedule(
+                start_date=default_start_date, end_date=default_end_date, team=team_id
+            )
+            date_and_team = [[0] * 2 for i in range(len(team_schedule) - 1)]
+            for schedule in range(0, 3):
+                date_and_team[schedule][0] = team_schedule[schedule]["game_date"]
+                date_and_team[schedule][1] = team_schedule[schedule]["away_name"]
+
+            with open("configure_package/mlb_i18n_team_list.json") as mlb_i18n_team:
+                away_team_list = json.load(mlb_i18n_team)
+
+            response_builder = {
+                "version": "2.0",
+                "resultCode": "OK",
+                "output": {
+                    "our_team_name": request_team,
+                    "return_game_date1": date_and_team[0][0],
+                    "return_away_name1": away_team_list[date_and_team[0][1]],
+                    "return_game_date2": date_and_team[1][0],
+                    "return_away_name2": away_team_list[date_and_team[1][1]],
+                    "return_game_date3": date_and_team[2][0],
+                    "return_away_name3": away_team_list[date_and_team[2][1]],
+                },
+            }
         else:
             with open("configure_package/team_and_teamid.json") as other_team:
                 other_team_list = json.load(other_team)
             team_id = other_team_list[request_team]
 
-        time = datetime.now()
-        default_start_date = time.strftime("%Y-%m-%d")
-        default_end_date = (time + timedelta(days=90)).strftime("%Y-%m-%d")
-        team_schedule = statsapi.schedule(
-            start_date=default_start_date, end_date=default_end_date, team=team_id
-        )
-        date_and_team = [[0] * 2 for i in range(len(team_schedule) - 1)]
-        for schedule in range(0, 3):
-            date_and_team[schedule][0] = team_schedule[schedule]["game_date"]
-            date_and_team[schedule][1] = team_schedule[schedule]["away_name"]
-        response_builder = {
-            "version": "2.0",
-            "resultCode": "OK",
-            "output": {
-                "our_team": request_team,
-                "return_game_date1": date_and_team[0][0],
-                "return_away_name1": date_and_team[0][1],
-                "return_game_date2": date_and_team[1][0],
-                "return_away_name2": date_and_team[1][1],
-                "return_game_date3": date_and_team[2][0],
-                "return_away_name3": date_and_team[2][1],
-            },
-        }
+            other_team_schedule_url = (
+                f"https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id={team_id}"
+            )
+            r = requests.get(other_team_schedule_url).text
+            three_events = json.loads(r)["events"][2:5]
+
+            response_builder = {
+                "version": "2.0",
+                "resultCode": "OK",
+                "output": {
+                    "our_team_name": request_team,
+                    "return_game_date1": three_events[-1]["dateEvent"],
+                    "return_away_name1": three_events[-1]["strAwayTeam"],
+                    "return_game_date2": three_events[-2]["dateEvent"],
+                    "return_away_name2": three_events[-2]["strAwayTeam"],
+                    "return_game_date3": three_events[0]["dateEvent"],
+                    "return_away_name3": three_events[0]["strAwayTeam"],
+                },
+            }
+
         return Response(response_builder)
